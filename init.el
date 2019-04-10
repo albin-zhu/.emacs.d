3c11f4598683c7a5ee19ccd88f4b528c5f891b2d
Albin <albin@AlbindeMacBook-Pro.local>
2019-04-10 15:26:05 +0800

    [feat] org-agenda

--- init.el
+++ init.el
@@ -6,6 +6,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/web.org" "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/inbox.org")))
  '(org-babel-load-languages
    (quote
     ((shell . t)
@@ -17,7 +20,7 @@
      (js . t))))
  '(package-selected-packages
    (quote
-    (dockerfile-mode treemacs docker docker-compose-mode eglot window-numbering use-package undo-tree smex pretty-mode paredit ox-jira org-timeline org-preview-html org-present org-pdfview org-bullets neotree multiple-cursors magit-org-todos magit-gitflow lsp-python ivy-rich ivy-hydra htmlize highlight-parentheses exec-path-from-shell doom-themes doom-modeline counsel company-lsp auto-complete)))
+    (importmagic which-key dockerfile-mode treemacs docker docker-compose-mode eglot window-numbering use-package undo-tree smex pretty-mode paredit ox-jira org-timeline org-preview-html org-present org-pdfview org-bullets neotree multiple-cursors magit-org-todos magit-gitflow lsp-python ivy-rich ivy-hydra htmlize highlight-parentheses exec-path-from-shell doom-themes doom-modeline counsel auto-complete)))
  '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
