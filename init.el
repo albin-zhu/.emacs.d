3e03a95ca519e79ccf7ed8cda96791ba989d4d36
albin <yabin_zhu@staff.9you.com>
2019-04-27 11:03:44 +0800

    [feat] Add input method, update translation plugin

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(add-comment t)
  '(org-agenda-files
    (quote
     ("~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/web.org" "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/inbox.org")))
@@ -20,7 +21,7 @@
      (js . t))))
  '(package-selected-packages
    (quote
-    (fontawesome dashboard youdao-dictionary go-mode lsp-go protobuf-mode omnisharp ob-mongo company-lsp zoom-window zoom window-numbering which-key use-package undo-tree treemacs solarized-theme smex paredit org-preview-html org-bullets multiple-cursors multi-term magit-org-todos magit-gitflow lsp-mode ivy-rich ivy-hydra htmlize highlight-parentheses exec-path-from-shell doom-themes doom-modeline dockerfile-mode docker-compose-mode docker counsel company)))
+    (chinese-wbim pyim-wbdict pyim fontawesome dashboard youdao-dictionary go-mode lsp-go protobuf-mode omnisharp ob-mongo company-lsp zoom-window zoom window-numbering which-key use-package undo-tree treemacs solarized-theme smex paredit org-preview-html org-bullets multiple-cursors multi-term magit-org-todos magit-gitflow lsp-mode ivy-rich ivy-hydra htmlize highlight-parentheses exec-path-from-shell doom-themes doom-modeline dockerfile-mode docker-compose-mode docker counsel company)))
  '(python-shell-interpreter "python2")
  '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
